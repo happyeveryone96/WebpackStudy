bdf0329215955b742ffecd634723749a775585a0
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-02-22 14:41:48 +0900

    using webpack-dev-server

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -8,6 +8,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
      print: './src/print.js',
    },
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
