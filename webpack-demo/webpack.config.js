9a6f8e7e9d13ae5349d70cd372669658aad0d246
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-02-22 11:22:17 +0900

    loading fonts

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -16,6 +16,10 @@ const path = require('path');
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
  };
