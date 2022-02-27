b6866019e347d4cc41ef4fc03c5e7f5e719ce04b
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-02-27 14:22:23 +0900

    authoring a library

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,16 +1,22 @@
 import _ from 'lodash';
-// import Print from './print';
+import numRef from './ref.json';
 
-async function getComponent() {
-  const element = document.createElement('div');
-  const { default: _ } = await import('lodash');
+export function numToWord(num) {
+  return _.reduce(
+    numRef,
+    (accum, ref) => {
+      return ref.num === num ? ref.word : accum;
+    },
+    ''
+  );
+}
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // element.onclick = Print.bind(null, 'Hello webpack!');
-  
-  return element;
- }
-
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+export function wordToNum(word) {
+  return _.reduce(
+    numRef,
+    (accum, ref) => {
+      return ref.word === word && word.toLowerCase() ? ref.num : accum;
+    },
+    -1
+  );
+}
